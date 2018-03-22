2017bd2a6cd55911a5529a33f8593b889ee83fa9
Rivera <J.C.RIVERA@cgiar.org>
2018-03-22 14:04:49 -0500

    jcr

--- FORMATRANDOMFOREST.R
+++ FORMATRANDOMFOREST.R
@@ -3,20 +3,16 @@
 #Arguments      -listFiles list of files
 
 
-match_files  <- function(listFiles, type)
+match_files  <- function(type)
 {
   
-  
+  slitingnames <- split_name(list.files(here("Rmawgen", "Files_By_Station")))
+  length <- seq(1, length(slitingnames ), by = 3)
+  names_stations <- unique(split_name(list.files(here("Rmawgen", "Files_By_Station")))[length])
 
-  filenames <- read.csv(paste0(here("Results"),"/", "Results_DailyControl.csv"), header=T)
-  names_stations <- unique(filenames$Station_Name)
-  
-  #Grouping according names station
-  #files  <- lapply(names_stations, merge_files, listfiles = list.files(), type = type)
-  files  <- lapply(names_stations, merge_files, listfiles = list.files(), type = type)
-  
+  files  <- lapply(names_stations, merge_files, listfiles = list.files(here("Rmawgen", "Files_By_Station")), type = type)
   return (files)
-  #return (names_stations )
+
 }
 
 #merge_files merges files according to names
@@ -24,20 +20,18 @@ merge_files <- function (name, listfiles, type)
 {
   name <- as.character(name)
   
-  files <-  list.files(pattern= name)
+  files <-  list.files(here("Rmawgen", "Files_By_Station"), pattern= name)
   read_files <- lapply(files,label_for_files)
   tablaPerday <- Reduce(function(dtf1, dtf2) merge(dtf1, dtf2, by = "Date", all = TRUE),read_files)
   
   namec <- paste0(name,".txt" )
-  #write.table(tablaPerday, file = paste(getwd(),"Random_Forest", namec, sep =  "/"), row.names = FALSE, quote = FALSE, col.names = TRUE)
-  if(type == 1)
+
+  if(type == "RandomForest")
   {
-    #write.table(tablaPerday, file = paste("../../RandomForest", namec, sep =  "/"), row.names = FALSE, quote = FALSE, col.names = TRUE)
     write.table(tablaPerday, file = paste0(here("RandomForest"), "/", namec), row.names = FALSE, quote = FALSE, col.names = TRUE)
   }
-  if(type == 2)
+  if(type == "Final_Data")
   {
-    #write.table(tablaPerday, file = paste0("../../Final_Data", namec, sep =  "/"), row.names = FALSE, quote = FALSE, col.names = TRUE)
     write.table(tablaPerday, file = paste0(here("Final_Data", namec)), row.names = FALSE, quote = FALSE, col.names = TRUE)
   }
   
@@ -48,52 +42,57 @@ merge_files <- function (name, listfiles, type)
 
 label_for_files <- function (name)
 {
-  file <- read.table(name, header=T)
+  file <- read.table(paste0(here("Rmawgen", "Files_By_Station"), "/", name), header=T)
   variable <-  split_name(name)[2]
   names(file) <- c("Date", variable)
   
   return(file) 
 } 
 
+#move_files_SR_HR works for copying data from AfterDailyControl_Data to  Files_By_Station
 
 move_files_SR_HR <- function ()
 {
-  #list_files_SR <- list.files("../../AfterDailyControl_Data", pattern ="_SR_")
-  #list_files_RH <- list.files("../../AfterDailyControl_Data", pattern ="_RH_")
-  
-  
+
   list_files_SR <- list.files(here("AfterDailyControl_Data"), pattern ="_SR_")
   list_files_RH <- list.files(here("AfterDailyControl_Data"), pattern ="_RH_")
   
-  lapply(list_files_SR, copy_paste_files)
-  lapply(list_files_RH, copy_paste_files)
   
-}
-
-#paste_name works for delete the last part of name. e.g. Chihauaita_SR_WAM2 converts to Chihauaita_SR
-paste_name <- function(namefile)
-{
-  split_na <-  split_name(namefile) [3] 
-  name_comple <- gsub(paste0("_",split_na),"",namefile)
-  return (name_comple)
-}
-
-#copy_extension works for making path of files
-copy_paste_files <- function(namefile)
-{
-
-  name_comp <- paste_name(namefile)
-  path_from <- paste0(here("AfterDailyControl_Data"), "/", namefile)
-  path_to <- paste0(here("Rmawgen", "Files_By_Station"), "/",name_comp)
-  file.copy(path_from, path_to)
+  for  (i in 1: length(list_files_SR))
+  {
+    path_from <- paste0(here("AfterDailyControl_Data"), "/", list_files_SR[i])
+    path_to <- paste0(here("Rmawgen", "Files_By_Station"), "/",list_files_SR[i])
+    file.copy(path_from, path_to)
+    
+  }
+  
+  for  (i in 1: length(list_files_RH))
+  {
+    name_comp <- list_files_RH[i]
+    path_from <- paste0(here("AfterDailyControl_Data"), "/", list_files_RH[i])
+    path_to <- paste0(here("Rmawgen", "Files_By_Station"), "/",list_files_RH[i])
+    file.copy(path_from, path_to)
+    
+  }
   
 }
 
 
 
 
+#The split_name function works split name file
+#-Argument: name file
+#-Return: split of name file 
 
-# w <- lapply(files, function(x){read.table(x, header=TRUE)})
-# 
-# lapply(name_estaciones, merge_files, listfiles =list.files() )
+split_name <- function(filename)
+{
+  split_name <- unlist(strsplit(filename, split='_', fixed=TRUE))
+  split_name <- gsub("txt","",gsub("[[:punct:]]","",split_name) )
+  
+  return(split_name)
+}
+
+
+
+
 
